0a4ba78123b007f64511eef285a5bf33f3cdb59d
henry leong <hleong25+github@gmail.com>
2020-04-27 14:06:12 -0700

    intermittent changes

--- src/typescript/ffcrypto.ts
+++ src/typescript/ffcrypto.ts
@@ -1,1 +1,8 @@
-console.log("hello world tsc");
+console.log("henry -- ");
+// log.setDefaultLevel('info')
+// log.setLevel('info', true)
+log.trace("ffcrypto loaded");
+log.debug("ffcrypto loaded");
+log.info("ffcrypto loaded");
+log.warn("ffcrypto loaded");
+log.error("ffcrypto loaded");
