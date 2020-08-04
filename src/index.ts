38b327ce499d0b5296037965507108435b1953c4
henry leong <hleong25+github@gmail.com>
2020-08-03 22:43:06 -0700

    remove main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import log from "loglevel";
-import { main } from "./main";
 
 log.enableAll(false);
 
 try {
     log.info("ffcrypto starting", (new Date).toISOString());
-
-    main();
 
 } catch (err) {
     log.error("Application crashed.", err);
