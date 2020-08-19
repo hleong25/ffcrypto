a1964a2e0f9498172071994c5aa94eb1503c7f4f
henry leong <hleong25+github@gmail.com>
2020-08-18 22:25:33 -0700

    add unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // transform: {
+  //   "^.+\\.[t|j]sx?$": "babel-jest"
+  // },
   setupFiles: [
-    './src/injections.ts',
-  ]
+    './src/inversify.config.ts',
+  ],
 };
---
+++ src/persist/LocalStorageFacade.test.ts
@@ -0,0 +1,16 @@
+import { LocalStorageFacade } from "./LocalStorageFacade";
+
+let localStorageFacade: LocalStorageFacade;
+
+describe("LocalStorageFacade tests", () => {
+
+    beforeEach(() => {
+        localStorageFacade = new LocalStorageFacade();
+
+    })
+
+    test("test", () => {
+
+        // localStorageFacade.persist("key", "value");
+    })
+})
